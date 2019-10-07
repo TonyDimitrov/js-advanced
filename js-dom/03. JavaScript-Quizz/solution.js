f63f1ce20ea9f08704078426f399a5b3d578d441
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-10-07 08:00:52 +0300

    work on quize 04. task

---
+++ js-dom/03. JavaScript-Quizz/solution.js
@@ -0,0 +1,30 @@
+function solve() {
+
+  const correctAnswers = {
+    0: 2,
+    1: 2,
+    2: 2
+  }
+
+  document.addEventListener("click", selectAnswer);
+
+  function selectAnswer(){
+
+    let quizSections = document.getElementsByTagName("section");
+
+    let hidden = quizSections.indexOf(hidden);
+
+    let answers;
+    isanswerCorrect()
+    {
+      
+  
+      quizSections.array.forEach(x => x.quizSections.hidden = true);
+  
+       
+    }
+  }
+
+
+
+}
