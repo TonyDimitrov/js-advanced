aa67c81eefe6c79865cb72a6f18cf3c1945ee256
Tony Dimitrov <01tonydimitrov@gmail.com>
2019-10-16 08:12:36 +0300

    calculator

--- js-dom/04. Numpad-Calculator/solution.js
+++ js-dom/04. Numpad-Calculator/solution.js
@@ -1,53 +1,75 @@
+
 function solve() {
-    const notNumericValues = ["Clear", "=", "+", "-", "*", "/"];
+
+    const calcByOperator = new Map();
+    calcByOperator.set("+", (a, b) => a + b);
+    calcByOperator.set("+", (a, b) => a - b);
+    calcByOperator.set("+", (a, b) => a * b);
+    calcByOperator.set("+", (a, b) => a / b);
 
     const clear = "Clear";
+    const equal = "=";
 
     let calcInput = {
         clear: false,
-        oper : [],
-        expression: "",
-        input: ""
+        equal: false,
+        expression: []
     }
 
-    getEvent(calcInput, notNumericValues);
+    getEvent(calcInput, calcByOperator);
 
 
-    function getEvent(calcInput, notNumericValues) {
+    function getEvent(calcInput, calcByOperator) {
 
         let btns = document.querySelectorAll("button");
+        let output = document.querySelector("#expressionOutput");
 
         btns.forEach(b => b.addEventListener("click", getValue))
 
-        function getValue(args, calcInput, notNumericValues) {
-            handleInput(args, calcInput, notNumericValues)
+        function getValue(args) {
+            let setInput = handleInput(args, calcInput);
+            visualizeExpression(setInput, output, calcByOperator);
         }
     }
 
-    function handleInput(args, calcInput, notNumericValues) {
+    function handleInput(args, calcInput) {
 
         let val = args.currentTarget.value;
-
-        if (Number(val)) {
-            var valNumber = Number(val);
-        } else if(notNumericValues.includes(val)){
-            if (clear === val) {
+        if (clear === val) {
             calcInput.clear = true;
+        }
+        else {
+            if (Number(val)) {
+                calcInput.expression.push(val);
+            } else if (val === equal) {
+                calcInput.equal = true;
+            } else if (calcInput.expression.length > 0) {
+                calcInput.expression.push(val);
             }
-            
-calcInput.
+        }
+
+        return calcInput;
+    }
+
+    function visualizeExpression(setInput, output, calcByOperator) {
+        if (setInput.clear) {
+            output.innerText = "";
+        } else if (setInput.equal) {
+            output.innerText = makeCalculation(setInput, calcByOperator);
+        } else {
+            output.innerText = setInput.expression.join(" ");
         }
     }
 
-    function clearInput(){
+    function makeCalculation(setInput, calcByOperator) {
+        let total = 0;
+        for (let index = 1; index < setInput.expression.length; index += 2) {
+            calcFunk = calcByOperator.get(index);
+            total += calcFunk(index-1, index+1);
+        }
 
-    }
-
-    function visualizeExpression() {
-
-    }
-
-    function makeCalculation() {
-
+        return total;
     }
 }
+// 1 + 6 + 6 / 9 *
+
